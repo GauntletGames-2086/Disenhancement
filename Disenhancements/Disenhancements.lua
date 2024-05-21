5dd019235f869ecfdd3dadbe967819b12e30a584
ItsFlowwey <73145114+GauntletGames-2086@users.noreply.github.com>
2024-05-20 20:20:19 -0800

    Add "Me First!" + update Disenhancement API

--- Disenhancements/Disenhancements.lua
+++ Disenhancements/Disenhancements.lua
@@ -1,7 +1,7 @@
 --- STEAMODDED HEADER
 --- MOD_NAME: Disenhancements
 --- MOD_ID: CursedCards
---- MOD_AUTHOR: [ItsFlowwey]
+--- MOD_AUTHOR: [ItsFlowwey, Victin]
 --- MOD_DESCRIPTION: Adds "Disenhancements" onto cards
 --- PREFIX: disenhance
 
@@ -9,9 +9,10 @@
 ------------MOD CODE -------------------------
 
 local mod_path = SMODS.current_mod.path
-SMODS.Atlas{key = "TestCurses", path = "TestCurses.png", px = 71, py = 95, atlas_table = "ASSET_ATLAS"}
+SMODS.Atlas{key = "Disenhancements", path = "Disenhancements.png", px = 71, py = 95}
+SMODS.Atlas{key = "MeFirst", path = "me-first.png", px = 71, py = 126}
 
--- Define SMODS.Disenhancements. Should swap to "SMODS.Disenhancement" at some point
+-- Define SMODS.Disenhancement
 SMODS.Disenhancements = {}
 SMODS.Disenhancement = SMODS.GameObject:extend {
 	obj_table = SMODS.Disenhancements,
@@ -24,7 +25,7 @@ SMODS.Disenhancement = SMODS.GameObject:extend {
 	set = "Disenhancement",
 	config = {},
 	pos = { x = 0, y = 0 },
-	atlas = 'TestCurses',
+	atlas = 'Disenhancements',
 	unlocked = true,
 	discovered = false,
 	colour = HEX("9151b5"),
@@ -34,11 +35,15 @@ SMODS.Disenhancement = SMODS.GameObject:extend {
 		-- Create "Disenhancement" pool if it doesn't already exist
 		if not G["P_DISENHANCEMENTS"] then G["P_DISENHANCEMENTS"] = {} end
 		G.P_DISENHANCEMENTS[self.key] = self
+		--Create "Disenhancement" P_CENTER_POOLS if it doesn't already exist
 		if not G.P_CENTER_POOLS["Disenhancement"] then G.P_CENTER_POOLS["Disenhancement"] = {} end
 		SMODS.insert_pool(G.P_CENTER_POOLS[self.set], self)
 		self.reverse_lookup[self.key:lower()..'_disenhancement'] = self.key
+		--Create "Disenhancement" sprite group if it doesn't already exist
 		if not G["shared_disenhancements"] then G["shared_disenhancements"] = {} end
 		G.shared_disenhancements[self.key] = Sprite(0, 0, G.CARD_W, G.CARD_H, G.ASSET_ATLAS[self.atlas] or G.ASSET_ATLAS["stickers"], self.pos)
+		--Creates soul_pos sprite as a separate object
+		if self.soul_pos then G.shared_disenhancements[self.key.."soul_pos"] = Sprite(0, 0, G.CARD_W, G.CARD_H, G.ASSET_ATLAS[self.atlas] or G.ASSET_ATLAS["stickers"], self.soul_pos) end
 	end,
 	process_loc_text = function(self)
 		-- Create "Disenhancement" description group if it doesn't already exist
@@ -81,6 +86,7 @@ function Card:set_disenhancement(_disenhancement, silent, immediate)
 			end
 		end
 	end
+	if self.ability.disenhancement == "me_first" then self.ability.forced_selection = true end
 end
 
 SMODS.Disenhancement{
@@ -98,6 +104,7 @@ SMODS.Disenhancement{
 	},
 	colour = HEX('9151b5'),
 	pos = { x = 0, y = 0 },
+	atlas = 'Disenhancements',
 }
 
 SMODS.Disenhancement{
@@ -109,26 +116,47 @@ SMODS.Disenhancement{
 			text = {
 				"Consumable cards cannot",
 				"be used whilst this card",
-				"is held in hand",
+				"is in hand",
 			},
 		},
 		label = "Clouded",
 	},
 	colour = HEX('9151b5'),
-	pos = { x = 0, y = 1 },
+	pos = { x = 1, y = 0 },
+	atlas = 'Disenhancements',
+}
+
+SMODS.Disenhancement{
+	name = "Me First!",
+	key = "me_first",
+	loc_txt = {
+		description = {
+			name = "Me First!",
+			text = {
+				"This card is",
+				"always selected",
+			},
+		},
+		label = "Me First!",
+	},
+	colour = HEX('9151b5'),
+	pos = { x = 1, y = 0 },
+	soul_pos = {x = 0, y = 0},
+	atlas = 'MeFirst',
 }
 
 SMODS.Consumable{
 	set = "Spectral",
 	name = "Test Disenhancement Applier",
 	key = "test_disenhancement_applier",
-	config = {extra = "clouded"},
+	config = {extra = "webbed"},
 	discovered = true,
 	loc_txt = {
 		name = "Test Disenhancement Applier",
 		text = {
 			"This Spectral card",
-			"applies Test Disenhancement",
+			"applies Test",
+			"Disenhancement",
 		},
 	},
 	use = function(self, area, copier)
@@ -154,6 +182,35 @@ local CardAreacan_highlight_ref = CardArea.can_highlight
 function CardArea:can_highlight(card)
 	if card.ability.disenhancement == "webbed" then return false end
 	return CardAreacan_highlight_ref(self, card)
+end
+
+local Gameupdate_draw_to_hand_ref = Game.update_draw_to_hand
+function Game:update_draw_to_hand(dt)
+	Gameupdate_draw_to_hand_ref(self, dt)
+	local cards_total = 0
+	local webbed_cards = 0
+	for i=1, #G.hand.cards do
+		cards_total = cards_total + 1
+		if G.hand.cards[i].ability.disenhancement and G.hand.cards[i].ability.disenhancement == "webbed" then webbed_cards = webbed_cards + 1 end
+	end
+	if #G.hand.cards == G.hand.config.card_limit and cards_total == webbed_cards then 
+		print("All webbed cards")
+		print("cards_total: "..tostring(cards_total)..", webbed_cards: "..tostring(webbed_cards))
+		G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+			delay = '0.3',
+            func = function()
+				G.STATE = G.STATES.GAME_OVER
+				if not G.GAME.won and not G.GAME.seeded and not G.GAME.challenge then 
+					G.PROFILES[G.SETTINGS.profile].high_scores.current_streak.amt = 0
+				end
+				G:save_settings()
+				G.FILE_HANDLER.force = true
+				G.STATE_COMPLETE = false
+                return true
+            end
+        }))
+	end
 end
 
 local Cardcan_use_consumeable_ref = Card.can_use_consumeable
